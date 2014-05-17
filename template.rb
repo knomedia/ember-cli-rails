981cdc6f2abbaf98ba2e2403271dadacc7bd67f4
Jason Madsen <jmadsen@instructure.com>
2014-05-16 23:21:26 -0600

    custom generate build.sh based on app_name
    clean up template to use bash build.sh file based on app_name

--- template.rb
+++ template.rb
@@ -1,12 +1,11 @@
 # This script is used with the Ruby on Rails' new project generator:
 #
-#     rails new my_app -m path/to/this/ember_cli_template.rb
+#     rails new my_app -m path/to/this/template.rb
 #
-# For more information about the template API, please see the following Rails
-# guide:
+# For more information about the template API, see:
+#    http://edgeguides.rubyonrails.org/rails_application_templates.html
 #
-#     http://edgeguides.rubyonrails.org/rails_application_templates.html
-# assumes:
+# this template assumes:
 #
 # npm installed
 # npm install -g bower
@@ -14,7 +13,6 @@
 
 # Install required gems
 gem "active_model_serializers"
-gem "nokogiri"
 
 # kill un-needed gems
 run "sed -i.bck '/turbolinks/d' Gemfile"
@@ -27,47 +25,42 @@ run "bundle install"
 # cleanup
 run "rm Gemfile.bck"
 
+ember_app = "#{@app_name}-ember"
+
 # create ember-cli app
-run "ember new ember-app"
-run "cd ember-app && npm link ember-cli"
+run "ember new #{ember_app}"
+run "cd #{ember_app} && npm link ember-cli"
 
 #don't track node_modules via git
-run "echo '/ember-app/node_modules/*' >> .gitignore"
+run "echo '/#{ember_app}/node_modules/*' >> .gitignore"
 
-# build AssetsController and default to it
-generate :controller, "Assets", "index"
-run "rm app/views/assets/index.html.erb"
-file "app/views/assets/index.html.erb", "<%# Ember Starting Point %>"
-
-route "root 'assets#index'"
+# build rails catch-all route
+route "get '*path' => redirect('/')"
 
 generate :serializer, "application", "--parent", "ActiveModel::Serializer"
 inject_into_class "app/serializers/application_serializer.rb", "ApplicationSerializer" do
   "  embed :ids, :include => true\n"
 end
 
-# pull rake tasks
-run "curl -o lib/tasks/ember_cli_rails.rake https://gist.githubusercontent.com/knomedia/2528c6d1054e71402a1f/raw/ember_cli_rails.rake"
+#create build.sh from template put in bin/
+template_dir = File.expand_path(File.dirname(__FILE__))
+build_file_path = template_dir + "/build.sh.erb"
+template = File.open(build_file_path, "rb") {|io| io.read}
 
-
-#generate application_template, and build ember app
-run "bin/rake ember_cli_rails:application_template"
-run "bin/rake ember_cli_rails:build"
+build_content = ERB.new(template).result(binding)
+File.open('bin/build.sh', 'w') { |file| file.write(build_content) }
+run "chmod a+x bin/build.sh"
 
 puts <<-MESSAGE
 
 ***********************************************************
 
-Your ember-cli app is located at: 'ember-app'
+Your ember-cli app is located at: '#{@app_path}/#{ember_app}'
 see: https://github.com/stefanpenner/ember-cli
 
-AssetsController#index is the root route to your ember app
+To build the ember project for deployment run
 
-Make changes to app/views/layouts/application_template.html
-
-To build the ember project run:
-
-`bin/rails ember_cli_rails:build`
+`./bin/build.sh`
 
 ***********************************************************
 
